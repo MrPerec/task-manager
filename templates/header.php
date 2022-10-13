d995a75b953229e8859b4c45cef60d7eaa643725
MrPerec <MrPerec@mail.ru>
2022-10-13 16:20:59 +0400

    testing session_start

--- templates/header.php
+++ templates/header.php
@@ -4,11 +4,20 @@ require_once ($_SERVER['DOCUMENT_ROOT'] . CORE);
 
 $isAuthorized = false;
 
+// var_dump($_POST);
+
+// if (isset($_POST) && $isAuthorized) {
+//     session_start();
+// }
+
 if (isset($_POST['login']) && isset($_POST['password'])) {
     include_once $_SERVER['DOCUMENT_ROOT'] . USERS;
     include_once $_SERVER['DOCUMENT_ROOT'] . PASSWORDS;
 
     if ((in_array($_POST['login'], $users) && in_array($_POST['password'], $passwords)) && (array_flip($users)[$_POST['login']] == array_flip($passwords)[$_POST['password']])) {
+        session_start();
+        $_SESSION['login'] = true;
+        
         $isAuthorized = true;
     }
 }
