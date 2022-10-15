d4c59d4a7e688cd78c3bb66a0e6d023c55ca27c4
MrPerec <MrPerec@mail.ru>
2022-10-15 14:36:39 +0400

    refactoring auth logic

--- templates/auth.php
+++ templates/auth.php
@@ -1,30 +1,26 @@
 <?php
 
-$serverRootPath = $_SERVER['DOCUMENT_ROOT'];
-$isLoginTry = isset($_POST['login']) && isset($_POST['password']);
-$inputLoginValue = '';
-$inputPassValue = '';
+if (isset($_GET['login'])) {
+    $inputLoginValue = '';
+    $inputPassValue = '';
 
-if ($isLoginTry) {
-    include_once $serverRootPath . USERS;
-    include_once $serverRootPath . PASSWORDS;
+    if (isset($_POST['login']) && isset($_POST['password'])) {
+        include_once $serverRootPath . USERS;
+        include_once $serverRootPath . PASSWORDS;
+
+        if ((in_array($_POST['login'], $users) && in_array($_POST['password'], $passwords)) && (array_flip($users)[$_POST['login']] == array_flip($passwords)[$_POST['password']])) {
+            include_once $serverRootPath . SUCC_MSG;
+            
+            $_SESSION['isAuthorized'] = true;
+        } else {
+            include_once $serverRootPath . ERR_MSG;
+
+            $_SESSION['isAuthorized'] = false;
+            $inputLoginValue = $_POST['login'];
+            $inputPassValue = $_POST['password'];
+        }
+    }?>
     
-    $loginMessage = '';
-
-    if ((in_array($_POST['login'], $users) && in_array($_POST['password'], $passwords)) && (array_flip($users)[$_POST['login']] == array_flip($passwords)[$_POST['password']])) {
-        $_SESSION['isAuthorized'] = true;
-        $loginMessage = $serverRootPath . SUCC_MSG;
-    } else {
-        $_SESSION['isAuthorized'] = false;
-        $inputLoginValue = $_POST['login'];
-        $inputPassValue = $_POST['password'];
-        $loginMessage = $serverRootPath . ERR_MSG;
-    }
-
-    include_once $loginMessage;
-}
-
-if (isset($_GET['login'])) {?>
     <div class="index-auth">
         <form action="?login=yes" method="post">
             <table width="100%" cellspacing="0" cellpadding="0">
