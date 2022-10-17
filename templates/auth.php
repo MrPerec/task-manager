deee5f42ef0ba2982ef20bbe4577804306c27e8f
MrPerec <MrPerec@mail.ru>
2022-10-17 12:25:03 +0400

    added logic, if user not login to show login input

--- templates/auth.php
+++ templates/auth.php
@@ -1,28 +1,26 @@
-<?php
+<div class="index-auth">
+    <form action="?login=yes" method="post">
+        <table width="100%" cellspacing="0" cellpadding="0">
 
-if (isset($_GET['login'])) {
-    ?>
-    <div class="index-auth">
-        <form action="?login=yes" method="post">
-            <table width="100%" cellspacing="0" cellpadding="0">
+            <?php if (!isLogin()) { ?>
                 <tr>
                     <td class="iat">
                         <label for="login_id">Ваш e-mail:</label>
                         <input id="login_id" size="30" name="login" value="<?=htmlspecialchars(islogin() ? '' : ($_POST['login'] ?? ($_COOKIE['user'] ?? '')))?>">
                     </td>
                 </tr>
-                <tr>
-                    <td class="iat">
-                        <label for="password_id">Ваш пароль:</label>
-                        <input id="password_id" size="30" name="password" type="password" value="<?=htmlspecialchars(islogin() ? '' : $_POST['password'] ?? '')?>">
-                    </td>
-                </tr>
-                <tr>
-                    <td><input type="submit" value="Войти"></td>
-                </tr>
-            </table>
-        </form>
-        <?php if (!empty($_POST)) isLogin() ? include_once $serverRootPath . SUCC_MSG : include_once $serverRootPath . ERR_MSG; ?>
-    </div>
-    <? 
-}
+            <? } ?>
+            
+            <tr>
+                <td class="iat">
+                    <label for="password_id">Ваш пароль:</label>
+                    <input id="password_id" size="30" name="password" type="password" value="<?=htmlspecialchars(islogin() ? '' : $_POST['password'] ?? '')?>">
+                </td>
+            </tr>
+            <tr>
+                <td><input type="submit" value="Войти"></td>
+            </tr>
+        </table>
+    </form>
+    <?php if (!empty($_POST)) isLogin() ? include_once $serverRootPath . SUCC_MSG : include_once $serverRootPath . ERR_MSG; ?>
+</div>
