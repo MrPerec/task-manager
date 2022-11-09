d00e5ed10bf252532b85698ce67f1d6fa0d708ff
MrPerec <MrPerec@mail.ru>
2022-11-09 16:44:34 +0400

    add user to DB logic

--- templates/register.php
+++ templates/register.php
@@ -1,60 +1,59 @@
 <?php
 
-if (isset($_GET['register'])) {
-    define("MIN_PASS_LENGTH", 6);
+define("MIN_PASS_LENGTH", 6);
 
-    /* try {
-        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      
-        $dbh->beginTransaction();
-        $dbh->exec("insert into staff (id, first, last) values (23, 'Joe', 'Bloggs')");
-        $dbh->exec("insert into salarychange (id, amount, changedate)
-            values (23, 50000, NOW())");
-        $dbh->commit();
-      
-      } catch (Exception $e) {
-        $dbh->rollBack();
-        echo "Ошибка: " . $e->getMessage();
-      } */
-}
+/* try {
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    $dbh->beginTransaction();
+    $dbh->exec("insert into staff (id, first, last) values (23, 'Joe', 'Bloggs')");
+    $dbh->exec("insert into salarychange (id, amount, changedate)
+        values (23, 50000, NOW())");
+    $dbh->commit();
+} catch (Exception $e) {
+    $dbh->rollBack();
+    echo "Ошибка: " . $e->getMessage();
+} */
+var_dump($_POST);
 
-
-/* if (isset($_POST['login']) && isset($_POST['password']) && isset($_POST['password_confirm'])) {
+if (isset($_POST['login']) && isset($_POST['password']) && isset($_POST['password_confirm'])) {
     if (strlen($_POST['password']) >= MIN_PASS_LENGTH && strlen($_POST['password_confirm']) >= MIN_PASS_LENGTH) {
         if (!strcmp($_POST['password'], $_POST['password_confirm'])) {
             echo 'Пароли совпадают.';
-            
-
-
+            connect()->prepare("INSERT INTO `home_work_20`.`users` (login, surname, name, middle_name) 
+                            VALUES (?, ?, ?, ?);")
+                    ->execute($_POST['login'], $_POST['surname'], $_POST['name'], $_POST['middle_name'])
+                    ->prepare("INSERT INTO `home_work_20`.`password` ('password', 'user_id') 
+                            VALUES (?, ?);")
+                    ->execute($_POST['login'], 3);
         } else {
             echo 'Пароли не совпадают.';
         }
     } else {
         echo 'Длина пароля должна быть минимум 6 знаков.';
     }
-} */
+}
+
 
 //простой запрос
-/* $stmt = connect()->query(
-    "SELECT * FROM `home_work_20`.`users`
-        LEFT JOIN passwords ON users.id = passwords.user_id;"
-    );
+// $stmt = connect()->query("SELECT * FROM `home_work_20`.`users` 
+//                                 LEFT JOIN passwords ON users.id = passwords.user_id;");
 
-// var_dump($stmt->fetchAll(PDO::FETCH_ASSOC));
-
-while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-    var_dump("$row[surname] $row[name] $row[middle_name] $row[login] $row[password]");
-} */
-
+// // var_dump($stmt->fetchAll(PDO::FETCH_ASSOC));
+// while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+//     var_dump("$row[surname] $row[name] $row[middle_name] $row[login] $row[password]");
+// }
 
 //Пример использования подготовленных запросов в PHP PDO:
-$stmt = connect()->prepare("SELECT * FROM `home_work_20`.`users` 
-                            LEFT JOIN passwords ON users.id = :id");
+// $stmt = connect()->prepare("SELECT * FROM `home_work_20`.`users` LEFT JOIN passwords ON users.id = :id");
+// $id = 'passwords.user_id';
+// $stmt->execute([':id' => 'passwords.user_id']);
+//или короче
+// $stmt = connect()->prepare("SELECT * FROM `home_work_20`.`users` LEFT JOIN passwords ON users.id = ?");
+// $id = 'passwords.user_id';
+// $stmt->execute([$id]);
 
-$id = 'passwords.user_id';
-$stmt->execute([':id' => $id]);
-
-var_dump($stmt->fetchAll(PDO::FETCH_OBJ));
+// var_dump($stmt->fetchAll(PDO::FETCH_OBJ));
 
 $stmt = null;
 ?>
