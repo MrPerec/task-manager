6aef961c6fbfc59a4ed2c6fa6398fc54372e04cd
MrPerec <MrPerec@mail.ru>
2022-11-11 09:44:40 +0400

    refactoring regist logic

--- templates/register.php
+++ templates/register.php
@@ -1,51 +1,20 @@
 <?php
 
-/* try {
-    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
-    $dbh->beginTransaction();
-    $dbh->exec("insert into staff (id, first, last) values (23, 'Joe', 'Bloggs')");
-    $dbh->exec("insert into salarychange (id, amount, changedate)
-        values (23, 50000, NOW())");
-    $dbh->commit();
-} catch (Exception $e) {
-    $dbh->rollBack();
-    echo "Ошибка: " . $e->getMessage();
-} */
-// var_dump($_POST);
-
-/* $hash = password_hash("rasmuslerdorf", PASSWORD_DEFAULT);
-var_dump($hash);
-var_dump(password_verify('rasmuslerdorf', $hash)); */
-
 if (isset($_POST['login']) && isset($_POST['password']) && isset($_POST['password_confirm'])) {
     if (strlen($_POST['password']) >= MIN_PASS_LENGTH && strlen($_POST['password_confirm']) >= MIN_PASS_LENGTH) {
         if (!strcmp($_POST['password'], $_POST['password_confirm'])) {
-            connect()->prepare("INSERT INTO `home_work_20`.`users`(surname, name, middle_name, login) VALUES (:surname, :name, :middle_name, :login)")
-                    ->execute([
-                        ':surname' => $_POST['surname'],
-                        ':name' => $_POST['name'],
-                        ':middle_name' => $_POST['middle_name'],
-                        ':login' => $_POST['login'], 
-                    ]);
+            connect()->prepare("INSERT INTO `home_work_20`.`users`(surname, name, middle_name, login) VALUES (?, ?, ?, ?)")
+                    ->execute([$_POST['surname'], $_POST['name'], $_POST['middle_name'], $_POST['login']]);
 
             $lastUserId = connect()->lastInsertId();
             $hashUserPasswd = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
-            // connect()->prepare("INSERT INTO `home_work_20`.`passwords`(password, user_id) VALUES (:password, :user_id)")
-            //         ->execute([
-            //             ':password' => $hashUserPasswd,
-            //             ':user_id' => $lastUserId, 
-            //         ]);
             connect()->prepare("INSERT INTO `home_work_20`.`passwords`(password, user_id) VALUES (?, ?)")
-                    ->execute([
-                        $hashUserPasswd,
-                        $lastUserId, 
-                    ]);
+                    ->execute([$hashUserPasswd, $lastUserId]);
 
             $lastUserId = null;
-
             include_once $serverRootPath . REG_SUCC_MSG;
+           
         } else {
             include_once $serverRootPath . REG_PASS_MATCH_ERR_MSG;
         }
@@ -53,28 +22,6 @@ if (isset($_POST['login']) && isset($_POST['password']) && isset($_POST['passwor
         include_once $serverRootPath . REG_PASS_LENGTH_ERR_MSG;
     }
 }
-
-
-//простой запрос
-// $stmt = connect()->query("SELECT * FROM `home_work_20`.`users` 
-//                                 LEFT JOIN passwords ON users.id = passwords.user_id;");
-
-// // var_dump($stmt->fetchAll(PDO::FETCH_ASSOC));
-// while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-//     var_dump("$row[surname] $row[name] $row[middle_name] $row[login] $row[password]");
-// }
-
-//Пример использования подготовленных запросов в PHP PDO:
-// $stmt = connect()->prepare("SELECT * FROM `home_work_20`.`users` LEFT JOIN passwords ON users.id = :id");
-// $id = 'passwords.user_id';
-// $stmt->execute([':id' => 'passwords.user_id']);
-//или короче
-// $stmt = connect()->prepare("SELECT * FROM `home_work_20`.`users` LEFT JOIN passwords ON users.id = ?");
-// $id = 'passwords.user_id';
-// $stmt->execute([$id]);
-
-// var_dump($stmt->fetchAll(PDO::FETCH_OBJ));
-
 
 ?>
 
