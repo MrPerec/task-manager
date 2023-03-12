bfe394ed3271d31a340b4aa3de5c6bf52922712a
MrPerec <MrPerec@mail.ru>
2023-03-13 00:13:43 +0400

    added logic display list messages

--- include/posts.php
+++ include/posts.php
@@ -1,17 +1,51 @@
 <?php if (isWriter()) {
+
+$userId = $_SESSION['userId'];
+
+$selectGetMessages = "select  msgs.id, 
+                              sctns.section, 
+                              title, 
+                              is_read as `isRead`
+                      from `home_work_20`.`messages` msgs
+                      join `home_work_20`.`sections` sctns on sctns.id = msgs.section_id
+                      where receiver_user_id = ?";
+
+$messagesData = sendQueryDB($selectGetMessages, [$userId])['responseData'];
+
+$readMessages = [];
+$unreadMessages = [];
+
+foreach ($messagesData as $messageData) {
+  if ($messageData['isRead']) {
+    $readMessages[] = $messageData;
+  } else {
+    $unreadMessages[] = $messageData;
+  }
+}
+
   ?>
   <p><a class='message-link_cursor-pointer' href=<?=URI_ADD_POSTS?>>Написать сообщение</a></p>
   <div class='flex-box'>
     <li><b class='text-color-red'>Непрочитанные</b>
-      <ul><a class='message-link_cursor-pointer'>Заголовок сообщения и название раздела 1</a></ul>
-      <ul><a class='message-link_cursor-pointer'>Заголовок сообщения и название раздела 2</a></ul>
-      <ul><a class='message-link_cursor-pointer'>Заголовок сообщения и название раздела 3</a></ul>
+      <?php
+        foreach ($unreadMessages as $message) {
+          $messageUri = URI_DISPLAY_POST . '?id=' . $message['id'];
+          ?>
+          <ul><a class='message-link_cursor-pointer' href=<?=$messageUri?>><?=cutString($message['title'])?> <b><?=$message['section']?></b></a></ul>
+          <?
+        }
+      ?>
     </li>
     
     <li><b class='text-color-green'>Прочитанные</b>
-      <ul><a class='message-link_cursor-pointer'>Заголовок сообщения и название раздела 1</a></ul>
-      <ul><a class='message-link_cursor-pointer'>Заголовок сообщения и название раздела 2</a></ul>
-      <ul><a class='message-link_cursor-pointer'>Заголовок сообщения и название раздела 3</a></ul>
+      <?php
+        foreach ($readMessages as $message) {
+          $messageUri = URI_DISPLAY_POST . '?id=' . $message['id'];
+          ?>
+          <ul><a class='message-link_cursor-pointer' href=<?=$messageUri?>><?=cutString($message['title'])?> <b><?=$message['section']?></b></a></ul>
+          <?
+        }
+      ?>
     </li>
   </div>
   <?
